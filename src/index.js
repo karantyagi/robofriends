a53e790bdd3a278b81a125a92b432552ea426ee9
karantyagi <tyagi.k@husky.neu.edu>
2018-05-26 12:37:19 -0400

    creating asynchronous action

--- src/index.js
+++ src/index.js
@@ -7,11 +7,13 @@ import 'tachyons';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import {createLogger} from 'redux-logger'
+import {thunkMiddleWare} from 'redux-thunk';
 import registerServiceWorker from './registerServiceWorker';
 import {searchRobots} from "./reducers";
 
 const logger = createLogger()
-const store = createStore(searchRobots, applyMiddleware(logger))
+const store =
+    createStore(searchRobots, applyMiddleware(thunkMiddleWare, logger))
 
 ReactDOM.render(
     <Provider store={store}>
