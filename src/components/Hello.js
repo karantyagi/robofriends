38946858a0aa384fa1b24ab1e9ada8e547953a40
karantyagi <tyagi.k@husky.neu.edu>
2018-05-26 09:41:12 -0400

    completed a simple react app without scroll

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './Hello.css';
-import './App.css'
+import './containers/App.css'
 
 // class Hello extends Component {
 // //     render() {
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App'
-import './App.css';
+import App from './containers/App'
+import './containers/App.css';
 import 'tachyons';
 
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-	<div className={'mr2'}>
+	<div className={'pa3'}>
 		<App/>
 	</div>
 	// <Hello msg={'test message...'}/>
